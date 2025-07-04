9251729a78dbf6e7065fb14ecbfca135d143efd6
Валерия <lerysi97.97@mail.ru>
2025-07-05 00:35:00 +0300

    Добавлен новый метод для ссылок

--- user-service/notification-service/src/main/java/com/example/notificationservice/service/NotificationService.java
+++
@@ -1,7 +0,0 @@
-package com.example.notificationservice.service;
-
-public interface NotificationService {
-    void sendEmail(String to, String subject, String text);
-    void UserCreated(String email);
-    void UserDeleted(String email);
-}
